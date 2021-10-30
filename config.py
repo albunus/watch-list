7973990cb416054d49a74e6727a902beaa18b8ac
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-30 11:37:05 +0300

    adding SQLAlchemy

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
     pass
